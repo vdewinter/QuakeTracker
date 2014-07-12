80611ccc32fe016b543658336b1c9a4235999021
Victoria de Winter <dewinter.victoria@gmail.com>
2014-07-12 09:18:34 -0700

    Added historical points to map

--- earth.py
+++ earth.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, request, flash
 # from flask_sockets import Sockets
 import model
+import json
 
 app = Flask(__name__)
 app.secret_key = 'secret_key' # change
@@ -17,23 +18,45 @@ def index():
     return render_template("index.html")
 # I want to make this all one page- how should I organize requests here
 
-@app.route('/read_db')
+@app.route("/read_quakes_from_db")
 # gets every quake from DB - loop thru and format in dictionary- send as json to js
-def read_db():
+def read_quakes_from_db():
     historical_quake_data = model.session.query(model.Quake).all()
-    response = {}
+    response_list = []
+
     for quake in historical_quake_data:
+        response = {}
+        
         response["id"] = quake.id
-        respsonse["properties"]["latitude"] = quake.latitude
-        respsonse["properties"]["longitude"] = quake.longitude
+        response["latitude"] = quake.latitude
+        response["longitude"] = quake.longitude
+
+        response["year"] = quake.year
+        response["month"] = quake.month
+        response["day"] = quake.day
+        response["hour"] = quake.hour
 
         magnitudes = [quake.magnitude_mw, quake.magnitude_ms, 
         quake.magnitude_mb, quake.magnitude_ml, quake.magnitude_mfa, 
         quake.magnitude_unk]
-        avg_magnitude = sum(magnitudes)/len(magnitudes) if len(magnitudes) > 0 else float('nan')
+        magnitudes_to_average = []
+        for i in magnitudes:
+            if i:
+                magnitudes_to_average.append(float(i))
+        if len(magnitudes_to_average) > 0:
+            avg_magnitude = sum(magnitudes_to_average)/float(len(magnitudes_to_average)) 
+        else:
+            avg_magnitude = None
+        response["magnitude"] = avg_magnitude 
 
-        repsone["properties"]["magnitude"] = avg_magnitude 
+        response_list.append(response)
 
+    return json.dumps(response_list)
+
+@app.route("/write_new_quakes_to_db")
+# add new data since last update to db
+def write_new_quakes_to_db():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/earth.js
+++ static/js/earth.js
@@ -1,28 +1,81 @@
+var width = 1250,
+    height = 830;
+
+var projection = d3.geo.mercator();
+
+var path = d3.geo.path()
+    .projection(projection);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+// colors quake points by magnitude
+var pathRamp = d3.scale.linear()
+    .domain([6,7,8,9])
+    .range(["yellow","orange","red","#0x800517"]);
+
+// var siteLabel = svg.aappend("svg:text")
+//     .attr("x", 20)
+//     .attr("y", 20)
+//     .text("Click for details");
+
 function displayMap() {
-    var width = 1250,
-        height = 830;
-
-    var projection = d3.geo.equirectangular();
-
-    var path = d3.geo.path()
-        .projection(projection);
-
-    var svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-    d3.json("world.json", function(error, world) {
+    d3.json("/static/world.json", function(error, world) {
         if (error) return console.error(error);
 
         svg.append("path")
             .datum(topojson.feature(world, world.objects.subunits))
             .attr("d", path);
     });
+
+    // overlay fault lines
+    // d3.json("/static/plate_boundaries.json", function(error, world) {
+    //     if (error) return console.error(error);
+
+    //     svg.append("path") // or insert?
+    //         .datum(topojson.object(data, data.objects.tec)) // binds data directly without computing a join
+    //         .attr("class", "tectonic")
+    //         .attr("d", path);
+    // });
 }
 
-// --------------------------------JQUERY/AJAX
+function displayHistoricalQuakes() {
+    d3.json("/read_quakes_from_db", function(error, points) {
+    if (error) return console.error(error);
+    console.log(points);
 
-// function clickPastQuakes(event, obj){
-//     ;
-//     }
+    // make pins for historical data
+    svg.selectAll(".pin")
+        .data(points)
+        .enter().append("circle", ".pin")
+        // radius proportional to magnitude of quake
+        .attr("r", function(d) {
+            return d.magnitude/2;
+        })
+        // lat and lon
+        .attr("transform", function(d) {
+            return "translate(" + projection ([d.longitude, d.latitude]) + ")";
+        })
+        .style("fill", function(d) {
+            return (pathRamp(d.magnitude));
+        });
+        // .on("click", function(d) {siteLabel.text(d.magnitude, d.month, d.day, d.year, d.hour, d.minute)});
 
+    // append to svg
+    });
+
+    // });
+
+}
+
+function displayRealtimeQuakes() {
+
+}
+
+// $.ajax({
+//     url: "/read_quakes_from_db",
+//     type : "GET",
+//     dataType: "json"
+// }).done(function(response) {
+//     console.log(response);
